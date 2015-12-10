8f4feec7807a57a3c247a8df72739d67e6647116
Patrick Lewis <Patrickml.pml@gmail.com>
2015-12-09 20:43:11 -0500

    Published All

---
+++ package.js
@@ -0,0 +1,28 @@
+Package.describe({
+  name: 'patrickml:admob',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: '',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
+  api.use('ecmascript');
+  api.addFiles('admob.js');
+});
+
+Cordova.depends({
+  'cordova-plugin-admob': '2.9.9'
+});
+
+Package.onTest(function(api) {
+  api.use('ecmascript');
+  api.use('tinytest');
+  api.use('patrickml:admob');
+  api.addFiles('admob-tests.js');
+});
